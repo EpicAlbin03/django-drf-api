b177f9347a731fa9d2d166bbd55ce59334ce5fd8
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-04-09 16:23:06 +0200

    new ui

--- studentplatform/forms.py
+++ studentplatform/forms.py
@@ -12,11 +12,19 @@ class CourseChoiceField(forms.ModelChoiceField):
 
 
 class StudentForm(forms.ModelForm):
-    course = CourseChoiceField(queryset=Course.objects.all(), empty_label=None)
+    course = CourseChoiceField(
+        queryset=Course.objects.all(),
+        empty_label=None,
+    )
 
     class Meta:
         model = Student
         fields = ["name", "email", "date_of_birth", "grade", "is_active", "course"]
         widgets = {
             "date_of_birth": forms.DateInput(attrs={"type": "date"}),
+            "is_active": forms.CheckboxInput(
+                attrs={
+                    "class": "size-4 shrink-0 rounded-sm bg-primary text-primary-foreground accent-primary border border-primary ring-offset-background focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                }
+            ),
         }
