4cc0f45bd6622f80f26f1bba1fe81078c05aeee2
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-04-09 11:47:46 +0200

    duplicate admin url

--- studentplatform/urls.py
+++ studentplatform/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 
 from . import views
 
 urlpatterns = [  # type: ignore
-    path("admin/", admin.site.urls),
     path("", views.home, name="home"),
     path("students/", views.student_list, name="student_list"),
     path("students/<int:student_id>/", views.student_detail, name="student_detail"),
