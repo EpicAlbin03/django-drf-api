08eba183f004e13503c3b4cf3fb2b41ea03e826f
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-04-09 16:31:06 +0200

    fixes

--- studentplatform/forms.py
+++ studentplatform/forms.py
@@ -19,12 +19,7 @@ class StudentForm(forms.ModelForm):
 
     class Meta:
         model = Student
-        fields = ["name", "email", "date_of_birth", "grade", "is_active", "course"]
+        fields = "__all__"
         widgets = {
             "date_of_birth": forms.DateInput(attrs={"type": "date"}),
-            "is_active": forms.CheckboxInput(
-                attrs={
-                    "class": "size-4 shrink-0 rounded-sm bg-primary text-primary-foreground accent-primary border border-primary ring-offset-background focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-                }
-            ),
         }
